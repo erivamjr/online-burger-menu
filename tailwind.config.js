2645af74f914e0c3c3f613295ebfa2c5ceef8d8d
José Erivam <erivamdev@gmail.com>
2024-04-24 10:44:18 -0300

    :lipstick: feat:add styles in cart modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['Roboto', 'sans-serif'],
+      },
       backgroundImage: {
         'home': "url('/assets/bg.png')"
       },
