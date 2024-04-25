23f6e546871ef4984d25475f2f965ace3f5807ac
José Erivam <erivamdev@gmail.com>
2024-04-25 17:58:29 -0300

    :recycle: refactor: add warn when add product in cart

--- scripts.js
+++ scripts.js
@@ -38,6 +38,19 @@ menu.addEventListener('click', (e) => {
 });
 
 function addToCart(name, price) {
+
+  Toastify({
+    text: "Adicionado com sucesso!",
+    duration: 3000,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+  }).showToast();
+
   const existingItem = cart.find((item) => item.name === name);
 
   if (existingItem) {
